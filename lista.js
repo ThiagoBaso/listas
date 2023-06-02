799b63c81aa399f330a561ac70eb82b41fa4033a
ThiagoBaso <74115428+ThiagoBaso@users.noreply.github.com>
2023-06-02 11:41:26 -0300

    Update lista.js

--- lista.js
+++ lista.js
@@ -1,6 +1,6 @@
 [
-  {"id":1,"nome":Arthur,"idade":6},
-  {"id":2,"nome":Thiago,"idade":17},
-  {"id":3,"nome":Matheus,"idade":16},
-  {"id":4,"nome":Alexandre,"idade":77}
+  {"id":1,"nome":"Arthur","idade":6},
+  {"id":2,"nome":"Thiago","idade":17},
+  {"id":3,"nome":"Matheus","idade":16},
+  {"id":4,"nome":"Alexandre","idade":77}
 ]
